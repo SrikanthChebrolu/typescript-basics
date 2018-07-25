5ea336cf213eac653c131a0f0a91e3d956915824
Srikanth Chebrolu <srikanthchebrolu3333@gmail.com>
2018-07-25 12:26:34 +0530

    initial

---
+++ hello.js
@@ -0,0 +1,5 @@
+function greeter(message) {
+    return "Hello, " + message;
+}
+var user = [1, 1];
+console.log(greeter(user));
---
+++ hello.ts
@@ -0,0 +1,7 @@
+function greeter(message: string){
+    return "Hello, "+ message;
+} 
+
+var user = "Srikanth";
+
+console.log(greeter(user));
